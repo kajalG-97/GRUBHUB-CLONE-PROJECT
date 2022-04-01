b7c6d4b57e776b13c8dd403c4df2c03cb4727db3
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-04-01 22:41:49 +0530

    home-sec

--- src/components/home-sec1/SearchBackgroundDiv.jsx
+++ src/components/home-sec1/SearchBackgroundDiv.jsx
@@ -1,5 +1,23 @@
 import { BImg, H1 } from "./style";
 import { useState } from "react";
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import SearchAppBar from "./SearchBar";
+
+// export default function ColorButtons() {
+//     return (
+//         <Stack direction="row" spacing={2}>
+//             <Button color="secondary">Secondary</Button>
+//             <Button variant="contained" color="success">
+//                 Success
+//             </Button>
+//             <Button variant="outlined" color="error">
+//                 Error
+//             </Button>
+//         </Stack>
+//     );
+// }
 
 export const SearchBackgroundDiv = () => {
     const [htag, setHtag] = useState("Who delivers in your neighborhood?");
@@ -10,8 +28,10 @@ export const SearchBackgroundDiv = () => {
         <BImg className="SearchBackground">
             <H1>{htag}</H1>
             {/* <H1>Who delivers in your neighborhood?</H1> */}
-            <button onClick={() => setHtag("Who delivers in your neighborhood?")}>a1</button>
-            <button onClick={() => setHtag("fghjkl")}>a2</button>
+            {/* <Button variant="text">Text</Button> */}
+            <button onClick={() => setHtag("Who delivers in your neighborhood?")}>Delivery</button>
+            <button onClick={() => setHtag("Who offers pickup near you?")}>Pickup</button>
+            <SearchAppBar/>
         </BImg>  
     );
 }
