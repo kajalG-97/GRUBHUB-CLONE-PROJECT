3aee3a0c6c23d75f6a8f119c74916d9e77aab53e
Jibin Babu <jibinbabu966@gmail.com>
2022-04-03 13:49:04 +0530

    align

--- src/components/Payment/Leftside.jsx
+++ src/components/Payment/Leftside.jsx
@@ -1,8 +1,7 @@
- 
- export const LeftSide=()=>{
-    return(
-        <>
-         <div style={{flex:"5",  height:"100%", border:"1px solid red"}}></div>
-        </>
-    )
-}
+export const LeftSide = () => {
+  return (
+    <>
+      <div style={{ flex: "5", height: "100%", border: "1px solid red" }}></div>
+    </>
+  );
+};
--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -4,11 +4,16 @@ import { RightSide } from "./Rightside";
 
 //payment page
 export const Payment = () => {
-  return <div>
-    <NavSignup/>
-    <div className="parent" style={{display:"flex" ,width:"100%" , height:"800px"}}>
-       <LeftSide/>
-       <RightSide/>
- </div>
-  </div>
+  return (
+    <div>
+      <NavSignup />
+      <div
+        className="parent"
+        style={{ display: "flex", width: "100%", height: "800px" }}
+      >
+        <LeftSide />
+        <RightSide />
+      </div>
+    </div>
+  );
 };
