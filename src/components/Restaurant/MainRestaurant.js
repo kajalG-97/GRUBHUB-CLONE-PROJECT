8b2183c0d390aaa3e7070b84c0922c645eca831c
Shubham Suresh Borade <spsb36@gmail.com>
2022-04-03 17:00:51 +0530

    minor changes

--- src/components/Restaurant/MainRestaurant.js
+++ src/components/Restaurant/MainRestaurant.js
@@ -3,8 +3,11 @@ import Navbar from "./Navbar";
 import Restro from "./Restro";
 import { Footer } from "../home-sec2/section2";
 import MenuAboutReviews from "./MenuAboutReviews";
+import GoToTop from "./GoToTop";
 
 export default function MainRestaurant() {
+
+
   return (
     <div className="App">
       <Navbar />
@@ -12,6 +15,7 @@ export default function MainRestaurant() {
       <Restro />
       <MenuAboutReviews />
       <Footer />
+      <GoToTop />
     </div>
   );
 }
